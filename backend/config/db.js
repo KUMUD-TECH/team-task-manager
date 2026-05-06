ce9f36ee6f23b0619b7cdbe2669e67b5a532f7e3
Kumud Verma <Kumud12333@gmail.com>
2026-05-06 13:16:29 +0530

    modified:   backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,22 +5,26 @@ console.log("DB_NAME:", process.env.DB_NAME);
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
 
-    if (err) {
-        console.error("Database connection failed:", err);
-        return;
-    }
-
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
     console.log("MySQL Connected");
 
+    connection.release();
+  }
+
 });
 
-module.exports = db;
+module.exports = pool;
